293c40b7761fe884cf738fa77ae7937c94ec40ed
Guenter Grossberger <guenter.grossberger@ca.com>
2017-11-01 19:37:30 -0400

    updated scripts

--- copy-files.sh
+++ copy-files.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-AGENT_FILE=./IntroscopeAgentFiles-NoInstaller10.5.1.8tomcat.unix.tar
+AGENT_FILE=./IntroscopeAgentFiles-NoInstaller10.5.1.14tomcat.unix.tar
 MATHAPP_DIR=../mathapp-java/deploy
 ACC_PACKAGE=./acc-controller-package.tar
 
-cp $AGENT_FILE $AGENT_FILE $MATHAPP_DIR/MathClient.war mathclient/
-cp $AGENT_FILE $AGENT_FILE $MATHAPP_DIR/MathSimpleBackend.war  mathsimple/
-cp $AGENT_FILE $AGENT_FILE $MATHAPP_DIR/MathComplexBackend.war  mathcomplex/
-cp $AGENT_FILE $AGENT_FILE $MATHAPP_DIR/MathProxy.war mathproxy/
+cp $AGENT_FILE $MATHAPP_DIR/MathClient.war mathclient/
+cp $AGENT_FILE $MATHAPP_DIR/MathSimpleBackend.war  mathsimple/
+cp $AGENT_FILE $MATHAPP_DIR/MathComplexBackend.war  mathcomplex/
+cp $AGENT_FILE $MATHAPP_DIR/MathProxy.war mathproxy/
